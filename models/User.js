7ec7497ca20fe6674d717f2744c8d1480524c829
Noemi Stirparo <noemistirparo80@gmail.com>
2026-04-22 13:23:21 +0200

    Riorganizzazione cartelle

---
+++ models/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(name, surname, email, id, role){
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.id = id;
+        this.role = role;
+        this.tickets = [];
+    }
+}
